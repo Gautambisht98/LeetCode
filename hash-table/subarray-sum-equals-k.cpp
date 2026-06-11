0b0fcd7f439de409dee18db56f0c2c92faf26d03
Gautam Bisht <bishtgautam98@gmail.com>
2026-06-11 12:36:16 +0530

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- hash-table/subarray-sum-equals-k.cpp
+++ hash-table/subarray-sum-equals-k.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-             unordered_map<int,int> mp;
-        int maxlen=0;
-        int sum=0;
-        int count=0;
-       for(int i=0;i<nums.size();i++){
-          sum+=nums[i];
-          if(sum==k){
-            maxlen=max(maxlen,i+1);
-            count++;
-          }
-          int rem=sum-k;
+        unordered_map<int, int> mp;
 
-          if(mp.find(rem)!=mp.end()){
-            int len=i-mp[rem];
-            maxlen=max(maxlen,len);
-            count++;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            if (sum == k) {
 
-          }
-          // edge case for zeroes and neagtives 
-          if(mp.find(sum)==mp.end()){
-            mp[sum]=i;
-          }
-       } 
-       return count;
+                count++;
+            }
+            int rem = sum - k;
+
+            if (mp.find(rem) != mp.end()) {
+
+                count += mp[rem];
+            }
+            // edge case for zeroes and neagtives
+            if (mp.find(sum) == mp.end()) {
+                mp[sum]++;
+            }
+        }
+        return count;
     }
 };
