9fd0fae3f11468b8f52dae7e17a856d0e0edef5b
Gautam Bisht <bishtgautam98@gmail.com>
2026-06-22 12:56:48 +0530

    LC-rearrange-characters-to-make-target-string [Runtime: N/A; Memory: N/A]

---
+++ string/rearrange-characters-to-make-target-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+     unordered_map<char,int> freq;
+     for(char ch:s){
+        freq[ch]++;
+     }   
+     for(char che:target){
+        freq[che]++;
+     }
+    for(int i=0;i<s.length();i++){
+        if
+    }
+    }
+};
