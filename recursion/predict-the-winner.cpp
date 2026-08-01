04adaa4bfda507a99a06667a9845424e0d5078bf
Gautam Bisht <bishtgautam98@gmail.com>
2026-08-02 00:02:45 +0530

    LC-predict-the-winner [Runtime: N/A; Memory: N/A]

--- recursion/predict-the-winner.cpp
+++ recursion/predict-the-winner.cpp
@@ -1,4 +1,8 @@
-int n = nums.size();
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+
+        int n = nums.size();
         vector<vector<int>> dp(n, vector<int>(n, 0));
 
         for (int i = 0; i < n; i++)
@@ -16,3 +20,5 @@ int n = nums.size();
         }
 
         return dp[0][n - 1] >= 0;
+    }
+};
