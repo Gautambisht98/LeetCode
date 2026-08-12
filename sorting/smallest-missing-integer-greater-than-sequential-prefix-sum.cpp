d33297150de07684b8c8e0b9210b6379ce273aa3
Gautam Bisht <bishtgautam98@gmail.com>
2026-08-12 18:00:32 +0530

    LC-smallest-missing-integer-greater-than-sequential-prefix-sum [Runtime: N/A; Memory: N/A]

---
+++ sorting/smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            } else {
+                break;
+            }
+        }
+        int i=0;
+        while(true){
+            bool found = false;
+            for (int j = 0; j < nums.size(); j++) {
+                if (sum + i == nums[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return sum + i;
+            }
+        }
+        i++;
+    }
+};
