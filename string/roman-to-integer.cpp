5544195c840262a7a6a8d34689b37b2bbf7b34a5
Gautam Bisht <bishtgautam98@gmail.com>
2026-08-17 23:45:07 +0530

    LC-roman-to-integer [Runtime: 15 ms; Memory: 13.4 MB]

--- string/roman-to-integer.cpp
+++ string/roman-to-integer.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char, int> mp {
-            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500},
-                {'M', 1000}
-        }
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
 
         int ans = 0;
+
         for (int i = 0; i < s.length(); i++) {
             ans += mp[s[i]];
+
             if (i + 1 < s.length() && mp[s[i]] < mp[s[i + 1]]) {
                 ans -= 2 * mp[s[i]];
             }
         }
+
         return ans;
     }
 };
