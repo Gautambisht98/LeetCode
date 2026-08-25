dd79184044c81a3e10b2d77c58ce2a688a196516
Gautam Bisht <bishtgautam98@gmail.com>
2026-08-25 19:28:19 +0530

    LC-smallest-missing-multiple-of-k [Runtime: N/A; Memory: N/A]

---
+++ array/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        for(num:nums){
+            freq[num]++;
+        }
+        int i=1;
+        while(k>0){
+            
+            if(freq[num]!=k*i){
+             i++;
+            }else{
+                int ans=k*i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
