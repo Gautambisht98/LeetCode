1fd37a879354d5a586670a9ed2a1ba62a7a2c7b8
Gautam Bisht <bishtgautam98@gmail.com>
2026-08-26 23:49:51 +0530

    LC-reverse-vowels-of-a-string [Runtime: N/A; Memory: N/A]

--- two-pointers/reverse-vowels-of-a-string.cpp
+++ two-pointers/reverse-vowels-of-a-string.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        int n=s.length();
-      int left=0;
-      int right=n-1;
-      string vowels ="aeiouAEIOU";
-      while(left<right){
-        if(vowels.find(s[left])==string::npos){
-            left++;
+        int n = s.length();
+        int left = 0;
+        int right = n - 1;
+        string vowels = "aeiouAEIOU";
+        while (left < right) {
+            if (vowels.find(s[left]) == string::npos) {
+                left++;
+            } else if (vowels.find(s[right]) == string::npos) {
+                right--;
+            } else {
+                swap(s[left], s[right]);
+                left++;
+                right--;
+            }
         }
-        else if(vowels.find(s[right])==string::npos){
-            right--;
-        }
-        else{
-            swap(s[left],s[right]);
-            left++;
-            right--;
-        }
-      }
-      return s;
+        return s;
     }
 };
