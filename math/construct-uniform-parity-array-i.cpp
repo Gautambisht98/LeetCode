81fb645bea37e1ea0dd2f8fc11b018bbc2e4f0e7
Gautam Bisht <bishtgautam98@gmail.com>
2026-09-02 23:46:58 +0530

    LC-construct-uniform-parity-array-i [Runtime: N/A; Memory: N/A]

---
+++ math/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        vector<int> nums2;
+        for(int i=0;i<nums.size();i++){
+        
+        }
+        return true;
+    }
+};
