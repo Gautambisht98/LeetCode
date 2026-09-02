a0131649c1776148cbd87196af8d6222f9cb45c4
Gautam Bisht <bishtgautam98@gmail.com>
2026-09-02 23:48:58 +0530

    LC-construct-uniform-parity-array-i [Runtime: 0 ms; Memory: 30.3 MB]

--- math/construct-uniform-parity-array-i.cpp
+++ math/construct-uniform-parity-array-i.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        vector<int> nums2;
-        for(int i=0;i<nums1.size();i++){
-        
-        }
+    
         return true;
     }
 };
